3efe8b5e084b6d2cb515d035835b15b8df543d95
mluc404 <199426019+mluc404@users.noreply.github.com>
2025-03-13 21:29:41 -0700

    feat: refine playTurn() and checkWinOrTie()
    - Prevented turnCount increment after a win or tie
    - Reset the game and tested a 2nd game. Worked as expected
    
    TODO:
    - Make gameController into IIFE
    - Refind invalid move handling

--- script.js
+++ script.js
@@ -17,13 +17,18 @@
 // Create gameboard object
 function createGameboard() {
   let arr = new Array(9).fill("");
+  let isMarkValid = false;
 
   let getBoard = () => arr;
   let placeMark = (position, mark) => {
-    if (arr[position] === "") arr[position] = mark;
-    else
+    if (arr[position] === "") {
+      arr[position] = mark;
+      isMarkValid = true;
+    } else {
+      isMarkValid = false;
       alert(`This spot ${position} is already taken. Choose a differnt spot`);
-    return arr;
+    }
+    return isMarkValid;
   };
 
   let reset = () => arr.fill("");
@@ -63,6 +68,7 @@ function gameController(board, playerX, playerO) {
   // because there cant be a winner under 4 turns
   let checkWinOrTie = () => {
     for (let con in winCondition) {
+      // Check if playerX wins
       let doesXwin = winCondition[con].every(
         (index) => board.getBoard()[index] === "x"
       );
@@ -72,9 +78,9 @@ function gameController(board, playerX, playerO) {
         console.log(`Winner is x after ${turnCount} turns`);
         console.log("Location: ", winCondition[con]);
         console.log(board.getBoard());
-
         return;
       }
+      // Check if playerO win
       let doesOwin = winCondition[con].every(
         (index) => board.getBoard()[index] === "o"
       );
@@ -99,21 +105,34 @@ function gameController(board, playerX, playerO) {
   // Check if game is over first
   // todo: need a better way to switch player, prevent invalid moves
   let playTurn = (n) => {
+    // if (turnCount >= 4) checkWinOrTie(); // why checkWinOrTie doesn't work properly out here, but works fine inside the below if() ???
+    // console.log(turnCount);
+    // console.log(`is game over: ${isGameOver}`);
+    // console.log(`winner: ${winner}`);
     if (!isGameOver && winner === null) {
       turnCount % 2 === 0
-        ? (currentPlayer = playerO)
-        : (currentPlayer = playerX);
-      board.placeMark(n, currentPlayer.mark);
-      if (turnCount >= 4) checkWinOrTie();
-      turnCount++;
+        ? (currentPlayer = playerX)
+        : (currentPlayer = playerO);
+      let isMoveValid = board.placeMark(n, currentPlayer.mark);
+      if (turnCount >= 4) checkWinOrTie(); // why checkWinOrTie only works well inside this if() ???
+      if (isMoveValid && !isGameOver) turnCount++; // check if the move is valid and game is not over before incrementing turnCount
     } else {
-      console.log(`turn count here: ${turnCount}`);
-      console.log("Yo game is alreay overrrrrrrrrrrr");
+      console.log(`final count: ${turnCount}`);
+      console.log(`is game over down here: ${isGameOver}`);
+      console.log(`winner down here: ${winner.name}`);
+      console.log("Yo game is alreay overr");
     }
   };
 
   let printBoard = () => board.getBoard();
-  let resetGame = () => board.reset();
+  let resetGame = () => {
+    board.reset();
+    // Even tho these 4 below only change inside playTurn(), they are still affected and need to be resetted
+    turnCount = 0;
+    isGameOver = false;
+    winner = null;
+    currentPlayer = playerX;
+  };
 
   // out here, turnCount should be 0, so no need to reset count
   return { playTurn, printBoard, resetGame };
@@ -123,16 +142,33 @@ let game = gameController(board, playerX, playerO);
 
 // Test game
 game.playTurn(4); // x
-game.playTurn(6); // o
+game.playTurn(5); // o
 game.playTurn(7); // x
 game.playTurn(2); // o
 game.playTurn(0); // x
 game.playTurn(1); // o
-game.playTurn(5); // x
-game.playTurn(8); // o
-game.playTurn(3); // x
-// Winner is x after 9 turns
+game.playTurn(6); // x
+game.playTurn(8); // o Winner is o after 7 turns
 console.log(game.printBoard());
+// game.playTurn(3); // x
 
 game.resetGame();
+console.log("==========================================");
 console.log(game.printBoard());
+
+// Test game 2 works as expected
+game.playTurn(0); // x
+game.playTurn(3); // o
+game.playTurn(1); // x
+game.playTurn(6); // o
+game.playTurn(2); // x Winner is x after 4 turns
+
+console.log(game.printBoard());
+
+// progress
+// update 1: when a win or tie is decided, if a player makes another turn it wont count
+// update 2: reset() includes game board, turnCount, isGameOver, winner, and currentPlayer. I tested it with a 2nd game and the game worked fine
+// update 3: turnCount starts at 0 and first player is X
+// existing issue 1: why checkWinOrTie() only works properly when it's inside "if (!isGameOver && winner === null) {...}". When it's inside, it tracks winner precisely. In the first game, winner is playerO after 7 turns. When I put checkWinOrTie() outside of "if (!isGameOver && winner === null) {...}" it takes 8 turns to find the winner.
+// existing issue 2: how to make gameController into an IIFE as the return is an object with 3 methods: printBoard(), playTurn() and reset(). How can i make a game passing the board and players parameters like I am doing with factory function gameController?
+// existing issue 3: what is a better way to decide player switching. my current method turnCount%2 is working properly but i want to find a better method
